35e11fae4ae54b7356dce0fc8bc38bd37a5e7d47
Bogdan Boksan <bogdan.boksan@ananas.rs>
2025-04-28 15:23:23 +0200

    chore: initial commit

---
+++ ios/AdvertisingId.swift
@@ -0,0 +1,8 @@
+@objc(AdvertisingId)
+class AdvertisingId: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
